577bda609da9adffec5f7cfcfad1eccfa8aa4bc1
Philip Urban <urban.philip@googlemail.com>
2013-03-27 06:00:14 +0100

    added plugin required files

---
+++ src/main/java/de/raidcraft/rcchat/RCChatPlugin.java
@@ -0,0 +1,39 @@
+package de.raidcraft.rcchat;
+
+import de.raidcraft.api.BasePlugin;
+import de.raidcraft.api.config.ConfigurationBase;
+import de.raidcraft.rcchat.listener.ChatListener;
+
+/**
+ * @author Philip
+ */
+public class RCChatPlugin extends BasePlugin {
+
+    public LocalConfiguration config;
+
+    @Override
+    public void enable() {
+
+        config = configure(new LocalConfiguration(this));
+
+        registerEvents(new ChatListener());
+    }
+
+    @Override
+    public void disable() {
+        //TODO: implement
+    }
+
+    @Override
+    public void reload() {
+        //TODO: override
+    }
+
+    public class LocalConfiguration extends ConfigurationBase<RCChatPlugin> {
+
+        public LocalConfiguration(RCChatPlugin plugin) {
+
+            super(plugin, "config.yml");
+        }
+    }
+}
---
+++ src/main/java/de/raidcraft/rcchat/listener/ChatListener.java
@@ -0,0 +1,10 @@
+package de.raidcraft.rcchat.listener;
+
+import org.bukkit.event.Listener;
+
+/**
+ * @author Philip
+ */
+public class ChatListener implements Listener {
+
+}
